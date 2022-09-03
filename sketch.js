00b8b8a72d50c36a6c708c7d5681ecbfd976bf5d
atifahIA <106875269+atifahIA@users.noreply.github.com>
2022-09-03 17:06:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+
+var trex ,trex_running;
+function preload(){
+trex_running=loadAnimation("trex1.png","trex3.png","trex4.png")  
+
+}
+function setup(){
+  createCanvas(600,200)
+  
+  trex=createSprite(50,180,20,50)
+ trex.addAnimation("running",trex_running)
+ trex.scale=0.5
+ ground=createSprite(200,190,400,5)
+}
+
+
+function draw(){
+  background("white")
+  if(keyDown("space")){
+    trex.velocityY=-10
+  }
+  trex.velocityY=trex.velocityY+0.8
+  trex.collide(ground)
+  drawSprites()
+
+}
